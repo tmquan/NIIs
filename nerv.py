75c252eca78dd40bf35c979a5faf62f6fcb352e9
Tran Minh Quan <quantm88@gmail.com>
2022-08-11 13:56:19 +0700

    strategy to ddp

--- nerv.py
+++ nerv.py
@@ -587,7 +587,7 @@ if __name__ == "__main__":
             checkpoint_callback, 
         ],
         accumulate_grad_batches=4, 
-        strategy="dp", #"horovod", #"deepspeed", #"ddp_sharded",
+        strategy="ddp", #"horovod", #"deepspeed", #"ddp_sharded",
         precision=16,  #if hparams.use_amp else 32,
         # amp_backend='apex',
         # amp_level='O3', # see https://nvidia.github.io/apex/amp.html#opt-levels
