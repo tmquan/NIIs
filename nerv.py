3a20ed82d05a52c4983dc60766437dc55748edcb
Quan Tran <qtran@talosix.com>
2022-09-07 20:54:22 +0700

    add GAN

--- nerv.py
+++ nerv.py
@@ -410,6 +410,19 @@ class NeRVLightningModule(LightningModule):
             ),
             nn.Tanh(),
         )
+
+        self.discriminator = nn.Sequential(
+            DenseNet201(
+                spatial_dims=2,
+                in_channels=1,
+                out_channels=1,
+                act=("ReLU", {"inplace": True}),
+                norm=Norm.INSTANCE,
+                # dropout_prob=0.5,
+                # pretrained=True, 
+            ),
+        )
+
         self.l1loss = nn.L1Loss(reduction="mean")
         
 
@@ -464,6 +477,18 @@ class NeRVLightningModule(LightningModule):
     def forward_frustum(self, image2d: torch.Tensor):
         frustum = self.frustum_net(image2d * 2. - 1.) * .5 + .5 #[0]# [0, 1] 
         return frustum
+
+    def discrim_step(self, fake_images: torch.Tensor, real_images: torch.Tensor):
+        real_logits = self.discriminator(real_images) 
+        fake_logits = self.discriminator(fake_images) 
+        real_loss = F.softplus(-real_logits).mean() 
+        fake_loss = F.softplus(+fake_logits).mean()
+        return real_loss + fake_loss 
+
+    def gen_step(self, fake_images: torch.Tensor, real_images: torch.Tensor):
+        fake_logits = self.discriminator(fake_images) 
+        fake_loss = F.softplus(-fake_logits).mean()
+        return fake_loss 
 
     def _common_step(self, batch, batch_idx, optimizer_idx, stage: Optional[str]='evaluation'):   
         _device = batch["image3d"].device
@@ -532,23 +557,62 @@ class NeRVLightningModule(LightningModule):
         cams_loss = self.l1loss(orgcam_ct, estcam_ct) \
                   + self.l1loss(orgcam_xr, reccam_xr) 
 
-        info = {f'loss': 1e0*im3d_loss + 1e0*im2d_loss + 1e0*cams_loss + 1e0*tran_loss} 
+        # info = {f'loss': 1e2*im3d_loss + 1e2*tran_loss + 1e2*im2d_loss + 1e2*cams_loss} 
         
         self.log(f'{stage}_im2d_loss', im2d_loss, on_step=(stage=='train'), prog_bar=True, logger=True, sync_dist=True, batch_size=self.batch_size)
         self.log(f'{stage}_im3d_loss', im3d_loss, on_step=(stage=='train'), prog_bar=True, logger=True, sync_dist=True, batch_size=self.batch_size)
         self.log(f'{stage}_cams_loss', cams_loss, on_step=(stage=='train'), prog_bar=True, logger=True, sync_dist=True, batch_size=self.batch_size)
         self.log(f'{stage}_tran_loss', tran_loss, on_step=(stage=='train'), prog_bar=True, logger=True, sync_dist=True, batch_size=self.batch_size)
+        
+        # return info
+        # train generator
+        if optimizer_idx == 0:
+            g_loss = self.gen_step(fake_images=estimg_ct, real_images=orgimg_xr)
+            self.log(f'{stage}_g_loss', g_loss, on_step=True, prog_bar=True, logger=True)
+            info = {f'loss': 1e0*im3d_loss + 1e0*tran_loss + 1e0*im2d_loss + 1e0*cams_loss
+                           + g_loss} 
+            return info
 
-        return info
+        # train discriminator
+        elif optimizer_idx == 1:
+            d_loss = self.discrim_step(fake_images=estimg_ct, real_images=orgimg_xr)
+            d_grad = self.compute_gradient_penalty(fake_samples=estimg_ct, real_samples=orgimg_xr)
+            self.log(f'{stage}_d_loss', d_loss, on_step=True, prog_bar=True, logger=True)
+            info = {f'loss': d_loss+10*d_grad} 
+            return info
 
-    def training_step(self, batch, batch_idx, optimizer_idx=None):
+    def compute_gradient_penalty(self, fake_samples, real_samples):
+        """Calculates the gradient penalty loss for WGAN GP"""
+        # Random weight term for interpolation between real and fake samples
+        alpha = torch.Tensor(np.random.random((real_samples.size(0), 1, 1, 1))).to(self.device)
+        # Get random interpolation between real and fake samples
+        interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True)
+        interpolates = interpolates.to(self.device)
+        d_interpolates = self.discriminator(interpolates)
+        # fake = torch.Tensor(real_samples.shape[0], 1).fill_(1.0).to(self.device)
+        fake = torch.ones_like(d_interpolates)
+        # Get gradient w.r.t. interpolates
+        gradients = torch.autograd.grad(
+            outputs=d_interpolates,
+            inputs=interpolates,
+            grad_outputs=fake,
+            create_graph=True,
+            retain_graph=True,
+            only_inputs=True,
+        )[0]
+        gradients = gradients.view(gradients.size(0), -1).to(self.device)
+        gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+        return gradient_penalty
+
+
+    def training_step(self, batch, batch_idx, optimizer_idx):
         return self._common_step(batch, batch_idx, optimizer_idx=optimizer_idx, stage='train')
 
     def validation_step(self, batch, batch_idx):
-        return self._common_step(batch, batch_idx, optimizer_idx=None, stage='validation')
+        return self._common_step(batch, batch_idx, optimizer_idx=0, stage='validation')
 
     def test_step(self, batch, batch_idx):
-        return self._common_step(batch, batch_idx, optimizer_idx=None, stage='test')
+        return self._common_step(batch, batch_idx, optimizer_idx=0, stage='test')
 
     def _common_epoch_end(self, outputs, stage: Optional[str]='common'):
         loss = torch.stack([x[f'loss'] for x in outputs]).mean()
@@ -564,7 +628,17 @@ class NeRVLightningModule(LightningModule):
         return self._common_epoch_end(outputs, stage='test')
 
     def configure_optimizers(self):
-        return torch.optim.RAdam(self.parameters(), lr=1e0*(self.lr or self.learning_rate))
+        opt_g = torch.optim.RAdam([
+            {'params': self.opacity_net.parameters()}, 
+            {'params': self.clarity_net.parameters()}, 
+            {'params': self.density_net.parameters()}, 
+            {'params': self.frustum_net.parameters()}, 
+        ], lr=1e0*(self.lr or self.learning_rate))
+        opt_d = torch.optim.RAdam([
+            {'params': self.discriminator.parameters()}
+        ], lr=1e0*(self.lr or self.learning_rate))
+        return opt_g, opt_d
+        # return torch.optim.RAdam(self.parameters(), lr=1e0*(self.lr or self.learning_rate))
         # return torch.optim.RAdam([
         #         {'params': self.opacity_net.parameters()}], lr=1e0*(self.lr or self.learning_rate)), \
         #        torch.optim.RAdam([
@@ -623,7 +697,7 @@ if __name__ == "__main__":
             lr_callback,
             checkpoint_callback, 
         ],
-        accumulate_grad_batches=3, 
+        # accumulate_grad_batches=3, 
         strategy="ddp_sharded", #"horovod", #"deepspeed", #"ddp_sharded",
         precision=16,  #if hparams.use_amp else 32,
         # amp_backend='apex',
